29f0651eb598aa50a0a68ab38fa3a2a31d5d0acf
qouteall <qouteall@163.com>
2025-11-23 19:35:36 +0800

    Update main.rs

--- server_hotswap_example/src/main.rs
+++ server_hotswap_example/src/main.rs
@@ -12,54 +12,22 @@ use tower_http::services::ServeDir;
 use tracing::{error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-#[derive(Serialize)]
-struct IndexResponse {
-    hello: String,
-}
-
-async fn index() -> impl IntoResponse {
-    Json(IndexResponse {
-        hello: "world".to_string(),
-    })
-}
-
-static COUNTER: Mutex<i32> = Mutex::new(0);
-
-async fn increment() -> impl IntoResponse {
-    let mut counter = COUNTER.lock().unwrap();
-
-    *counter += 1;
-    // *counter *= 2;
-
-    Json(*counter)
-}
-
-// https://github.com/DioxusLabs/dioxus/pull/4588
-
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
-    dioxus_devtools::serve_subsecond(sub_main).await;
+    dioxus_devtools::serve_subsecond(router_main).await;
 }
 
-async fn sub_main() {
-    log::info!("Doing initialization");
+async fn router_main() {
+    use axum::{Router, routing::get};
 
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/increment", post(increment));
+    let app = Router::new().route("/", get(test_route));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    info!("listening on http://{}", listener.local_addr().unwrap());
-    if let Err(err) = axum::serve(listener, app).await {
-        error!("server error: {}", err);
-    }
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app.clone()).await.unwrap()
+}
+
+async fn test_route() -> axum::response::Html<&'static str> {
+    "axum works!!!!!".into()
 }
