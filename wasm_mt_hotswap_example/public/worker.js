61c36e36545afabc9663747689886d9ea04dcba9
qouteall <qouteall@163.com>
2025-12-05 20:59:53 +0800

    .

--- wasm_mt_hotswap_example/public/worker.js
+++ wasm_mt_hotswap_example/public/worker.js
@@ -1,5 +1,5 @@
-// synchronously, using the browser, import out shim JS scripts
-import wbg_init, { child_entry_point } from './raytrace_parallel.js';
+
+import wbg_init, { child_entry_point } from './wasm_mt_hotswap_example.js';
 
 // Wait for the main thread to send us the shared module/memory. Once we've got
 // it, initialize it all with the `wasm_bindgen` global we imported via
